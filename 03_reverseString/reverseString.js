2d53220e211d728c868949090847ad6865e42583
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-05-28 13:09:57 -0400

    Finished exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(aString) {
+    let reversed = "";
+    for (let i=0; i<aString.toString().length; i++) {
+        reversed = aString[i] + reversed;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
