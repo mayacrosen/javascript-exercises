cc26e38ef16ceb7710ddf8db56ea48470fa283b8
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-05-29 12:08:08 -0400

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
+    let fixed = args[0];
+   
+    for (let eraserIndex = 1; eraserIndex<args.length; eraserIndex++) {
+        eraser = args[eraserIndex];
+        function valueFilter(value) {
+            return value != eraser;
+        }
+        fixed = fixed.filter(valueFilter);
+
+    }
+    return fixed;
 };
 
+console.log(removeFromArray([1234], 1, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
