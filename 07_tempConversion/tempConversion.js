7e7048b2ee8e8a71bb5c9fd8ae767bed7250f07a
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-05-29 13:00:16 -0400

    Need to make a changes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahreneit) {
+  let cTemp = (fahreneit - 32) * 5 / 9;
+  return (Math.round(10 * cTemp) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fTemp = (celcius * 9 / 5) + 32;
+  return ((Math.round(10 * fTemp)) / 10);
 };
+
+console.log(convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
