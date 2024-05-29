3ef3d884eb19f5cfe9bba7963cd2fc0e30a0c1f4
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-05-29 12:32:49 -0400

    Finished exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if ((start < 0) || (end < 0) || 
+    (typeof start != "number") || (typeof end != "number")) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    for (let a=start; a<=end; a++) {
+        sum = sum + a;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
