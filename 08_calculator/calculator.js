7b08d64aa158eda844cb7f81733a96dc054d74d6
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-06-17 13:11:07 -0400

    finished excersize

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let raised = 1;
+  for (i=0; i<b; i++) {
+    raised = raised * a;
+  }
+	return raised;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorialed = 1;
+  for (let i=1; i<=num; i++) {
+    factorialed = factorialed * i;
+  }
+  return factorialed;
 };
 
 // Do not edit below this line
