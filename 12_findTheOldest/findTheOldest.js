d30d456adc1ca995fb875a3451476d3cf513b71e
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-06-20 10:07:37 -0400

    Still gives undefined but replit works

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let largest = people.reduce(function(big, person) {
+        let tempAge = person.yearOfDeath - person.yearOfBirth;
+        if (tempAge > (big.yearOfDeath - big.yearOfBirth)) {
+            return person;
+        }
+        // below is the line that fixed the weird 
+        return big;});
 
+    return largest.name;
 };
 
 // Do not edit below this line
