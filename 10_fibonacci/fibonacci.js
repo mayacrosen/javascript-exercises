9f0c647f4224df9772d16937849422836e24e609
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-06-20 09:20:14 -0400

    Finished exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let sequence = [0];
+    for (let i=1; i<=num + 1; i++) {
+        if (sequence.length <= 2) {
+            sequence.push(1);
+        }
 
+        else {
+            sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+        }
+    }
+    return sequence[num];
 };
 
 // Do not edit below this line
