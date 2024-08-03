ca57b986f3bb0d96366e8e894a6b5b824c27668c
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-08-03 16:08:11 -0400

    redid successfully

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,10 @@
 const reverseString = function(aString) {
-    let reversed = "";
-    for (let i=0; i<aString.toString().length; i++) {
-        reversed = aString[i] + reversed;
-    }
-    return reversed;
+    let reverseReverse = "";
+    aString.split("").forEach(function(item, index, array) {
+        reverseReverse = item + reverseReverse;
+        console.log(item);
+    });
+    return reverseReverse;
 };
 
 // Do not edit below this line
