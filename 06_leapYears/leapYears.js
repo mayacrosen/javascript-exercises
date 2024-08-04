087968ce40a978e97258781f3a140c923ab150f3
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-08-04 16:40:45 -0400

    redone

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,16 +2,15 @@ const leapYears = function(year) {
     if (year % 4 == 0) {
         if (year % 100 == 0) {
             if (year % 400 == 0) {
-                return true;
+                return true
             }
             return false;
         }
-
-        return true;
+        return true
     }
     return false;
 
-};
+}
 
 // Do not edit below this line
 module.exports = leapYears;
