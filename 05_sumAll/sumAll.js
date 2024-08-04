daa54236ae825c4239ce0d7d74b44b2552d2e5b4
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-08-04 16:27:14 -0400

    redone

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,24 +1,12 @@
-/* const sumAll = function(start, end) {
-    if ((start < 0) || (end < 0) || 
-    (typeof start != "number") || (typeof end != "number")) {
-        return "ERROR";
-    }
-    let sum = 0;
-    if (start > end) {
-        [start, end] = [end, start];
-    }
-    for (let a=start; a<=end; a++) {
-        sum = sum + a;
-    }
-
-    return sum;
-};
-*/
 const sumAll = function(start, end) {
     sum = 0;
-    if (end > start) {
+    if ((typeof end !== "number") || (typeof start !== "number") || (end < 0) || (start < 0)) {
+        return "ERROR";
+    }
+    if (end < start) {
         [end, start] = [start, end];
     }
+
     for (let i=start; i<=end; i++){
         sum += i;
     }
