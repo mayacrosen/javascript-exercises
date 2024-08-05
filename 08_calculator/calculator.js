130d954f1d6647b75232d85959570eaa81476a34
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-08-05 10:05:20 -0400

    not done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,35 +1,33 @@
 const add = function(a, b) {
-  return a + b;
-	
+	return a + b;
 };
 
 const subtract = function(a, b) {
-  return a - b;
-	
+	return a - b;
 };
 
 const sum = function(arr) {
-  return arr.reduce((sum, current) => sum + current, 0);
+  arr.reduce((sum, current) => sum + current);
 };
 
 const multiply = function(arr) {
-  return arr.reduce((sum, current) => sum * current, 1);
+  arr.reduce((product, current) => product * current);
 };
 
 const power = function(a, b) {
-  let raised = 1;
-  for (i=0; i<b; i++) {
-    raised = raised * a;
+  let finalNum = 1;
+  for (let i = 0; i < b; i++) {
+    finalNum = finalNum * a;
   }
-	return raised;
+  return finalNum;
 };
 
 const factorial = function(num) {
-	let factorialed = 1;
-  for (let i=1; i<=num; i++) {
-    factorialed = factorialed * i;
+  let finalNum = 0;
+  for (let i = 1; i <= num; i++) {
+    finalNum += i;
   }
-  return factorialed;
+	return finalNum;
 };
 
 // Do not edit below this line
