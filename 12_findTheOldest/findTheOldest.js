2abea46f8db9164ebc0265beeeca54ff0ea49e9a
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-08-06 10:46:26 -0400

    redone

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,20 @@
-const findTheOldest = function(people) {
-    let largest = people.reduce(function(big, person) {
-        let tempAge = person.yearOfDeath - person.yearOfBirth;
-        if (tempAge > (big.yearOfDeath - big.yearOfBirth)) {
+const findTheOldest = function(peeps) {
+    function getAge(peep) {
+        if (!peep.yearOfDeath) {
+            return (new Date().getFullYear()) - peep.yearOfBirth;
+        }
+        return peep.yearOfDeath - peep.yearOfBirth;
+    }
+    let old = peeps.reduce(function(prev, person) {
+        if (getAge(prev) < getAge(person)) {
             return person;
         }
-        // below is the line that fixed the weird 
-        return big;});
-
-    return largest.name;
-};
+        else {
+            return prev;
+        }
+    });
+    return old;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
